00fa71cfe3cd8612482fb2a46c7e1a10cdae037c
Chi <15520065@gm.uit.edu.vn>
2018-02-26 21:15:37 +0700

    Test

---
+++ src/Controller/Cell.java
@@ -0,0 +1,31 @@
+package Controller;
+
+/**
+ * Created by phanh on 2/26/2018.
+ */
+//Each cell have location x and y
+public class Cell {
+    private int x;
+    private int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    void setY(int y) {
+        this.y = y;
+    }
+}
